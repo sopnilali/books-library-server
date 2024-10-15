0c65e1dac9dce94f918ee1c2abc4f8ac65b4d3c3
sopnilali <sopnilali24@gmail.com>
2024-10-15 19:07:57 +0600

    added

---
+++ src/controllers/BorroBooks/Get/borrowBooksList.js
@@ -0,0 +1,18 @@
+const borowbooks = require("../../../models/borowbooks");
+
+const borrowBooksList = async (req, res) => {
+    if (req?.user?.email !== req?.query?.email) {
+      return res.status(403).send({ message: 'forbidden access' })
+    }
+    let query = {};
+    if (req.query?.email) {
+        query = { email: req.query.email }
+    }
+    const result = await borowbooks.find(query);
+    res.send(result)
+
+    console.log(req.user.email )
+}
+
+
+module.exports = borrowBooksList;
--- src/routes/borowbooks/index.js
+++ src/routes/borowbooks/index.js
@@ -1,25 +1,13 @@
 const express = require('express');
 const AddBorrowBooks = require('../../controllers/BorroBooks/Add/addBorrowBooks');
-const borrowBooks = require('../../controllers/BorroBooks/Get/borrowBooks');
 const borrowDeletes = require('../../controllers/BorroBooks/Deletes/borrowDeletes');
-const borowbooks = require('../../models/borowbooks');
 const verifyTaken = require('../../middlewares/verifyTaken');
+const borrowBooksList = require('../../controllers/BorroBooks/Get/borrowBooksList');
 const router = express.Router();
 
 
 // find all carts from database
-router.get('/api/borrow-books', verifyTaken, async (req, res) => {
-    if (req.user.email !== req.query.email) {
-        return res.status(403).send({ message: 'forbidden access' })
-    }
-    let query = {};
-    if (req.query.email) {
-        query = { email: req.query.email }
-    }
-    const result = await borowbooks.find(query);
-    res.send(result)
-    
-})
+router.get('/api/borrow-books', verifyTaken, borrowBooksList )
 
     // insert cart item from database
 router.post('/api/borrow-books', AddBorrowBooks)
